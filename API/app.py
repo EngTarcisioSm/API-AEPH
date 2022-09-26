d2bbd82b3029285a18513876821e6044ccd1437d
EngTarcisioSM <dev.engtsm@gmail.com>
2022-09-26 17:16:59 -0300

    requisicao get sobre indicares retornando valores de lista

--- API/app.py
+++ API/app.py
@@ -1,25 +1,33 @@
-# 1. Imports necessários para criação da rest API
 from flask import Flask
 from flask_restful import Resource, Api
 
-# 2. Instanciação do Objeto
 app = Flask(__name__)
 api = Api(app)
 
-# 4. Criação do recurso base da rest api
+# 1. lista basica de indicadores fixada em uma lista
+indicadores = [
+    {
+        'indicador_id': 1,
+        'nome_indicador': 'chao_fabrica'
+    },
+    {
+        'indicador_id': 2,
+        'nome_indicador': 'externo'
+    },
+    {
+        'indicador_id': 3,
+        'nome_indicador': 'recebimento'
+    }
+]
 
 
 class Indicadores(Resource):
 
-    # 5. teste do recurso inicial get
     def get(self):
-        # 6. Apenas Retorna uma mensagem simples
-        return {'indicadores': 'meus indicadores'}
+        # 2. entregando todos os indicadores registrados
+        return {'indicadores': indicadores}
 
 
-
-# 7. adicionando primeiro recurso da api, '/indicadores', devendo ser chamado 
-# dessa forma na requisição
 api.add_resource(Indicadores, '/indicadores')
 
 
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 001_PrimeiraIteracao
 - Teste de primeira iteração com a API realizada atravez do metodo get sobre o recurso hoteis 
     ![teste_get_indicadores](/img/readme_img/0001.png)
+
+# 002_ListarIndicadoresSDB
+- Listar atraves do comando get em indicares todos os indicadores do sistema 
+- Atraves da requisição get em indicadores, todos os indicadores estão sendo listados 
+    ![teste_get_indicadores_lista](/img/readme_img/0002.png)
+- Os indicadores listados estão armazenados em uma lista não sendo utilizado um banco de dados
