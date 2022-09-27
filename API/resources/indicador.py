89ebb89a9e30e62721a3debf06739e905d08523a
EngTarcisioSM <dev.engtsm@gmail.com>
2022-09-27 10:29:00 -0300

    clear new includes

--- API/resources/indicador.py
+++ API/resources/indicador.py
@@ -1,5 +1,3 @@
-# 1. Para conseguir receber novos indicadores é necessário incluir uma
-# biblioteca para tratar requisições "reqparse"
 from flask_restful import Resource, reqparse
 
 indicadores = [
@@ -17,12 +15,8 @@ indicadores = [
     }
 ]
 
-# 8. criacao do parse para receber os dados enviados pela requisicao
 argumentos = reqparse.RequestParser()
 argumentos.add_argument('nome_indicador')
-
-# 9. funcao auxiliar temporaria de checagem do ultimo id da lista, futuramente
-# essa função vem a ser modificada
 
 
 def check_last_id():
@@ -32,8 +26,6 @@ def check_last_id():
         last = indicador['indicador_id']
     return last
 
-
-# 10. checa se um nome de indicador ja existe
 def check_name(nome: str):
     global indicadores
     answer = None
@@ -50,25 +42,17 @@ class Indicadores(Resource):
         return {'indicadores': indicadores}
 
 
-# 2. É criado um novo recurso chamado IndicadorID para tratar cada indicador de
-# forma totalmente individualizada
-
-
 class IndicadorID(Resource):
-    # 4. pesquisa efetuada no banco de dados por indicadores baseada em
-    # indicador_id
+
     def get(self, indicador_id):
         try:
             id = int(indicador_id)
         except:
-            # 7. Caso o Parametro não seja possivel de ser convertido não é
-            # retornado nada
             return None
         for indicador in indicadores:
             if indicador['indicador_id'] == id:
                 return indicador
 
-    # 11. inclusão do metodo post
     def post(self, indicador_id):
         dados = argumentos.parse_args()
         ultimo_id = check_last_id()
@@ -83,19 +67,14 @@ class IndicadorID(Resource):
         return {'message': 'not included'}
 
 
-# 3. É criado um novo recurso chamado IndicadorNome para tratar cada indicador
-# de forma totalmente individualizada
-
 
 class IndicadorNome(Resource):
+
     def get(self, nome_indicador):
-        # 5. pesquisa efetuada no banco de dados por indicador baseada em
-        # nome_indicador
         for indicador in indicadores:
             if indicador['nome_indicador'] == nome_indicador:
                 return indicador
 
-    # 12. inclusão do metodo post
     def post(self, nome_indicador):
         dados = argumentos.parse_args()
         if dados['nome_indicador'] != nome_indicador:
